b535a72e51a75217a06ac5adc1eb77824234c497
kazukinakamuraKN <kajirikajiri@gmail.com>
2021-07-23 17:55:22 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import express from 'express'
+import mysql from 'mysql'
+
+const connection = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'admin',
+  database: 'test'
+});
+
+connection.connect()
+
 const app: express.Express = express()
 
 // Constants
@@ -7,6 +18,18 @@ const HOST = '0.0.0.0';
 
 // App
 app.get('/', (req, res) => {
+  const sql = 'select * from user'
+  connection.query(sql, (err, rows, fields)=> {
+    if (err) {
+      console.log(err)
+      return
+    }
+    for (const i in rows) {
+      console.log(fields)
+      console.log(rows[i])
+    }
+  })
+
   res.send('Hello World 3');
 });
 
